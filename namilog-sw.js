3403bb82742d421882cfd0175db52c58b4998f92
Antisanogile <kanaro.sano@gmail.com>
2026-05-09 08:58:59 +0900

    Add files via upload

--- namilog-sw.js
+++ namilog-sw.js
@@ -1,4 +1,4 @@
-const VERSION = 'namilog-sw-v12';
+const VERSION = 'namilog-sw-v13';
 self.addEventListener('install', (event) => { self.skipWaiting(); });
 self.addEventListener('activate', (event) => { event.waitUntil(self.clients.claim()); });
 self.addEventListener('notificationclick', (event) => {
