2782e3f35c3b7024bb10c743dcaa6eb354e94dad
Antisanogile <kanaro.sano@gmail.com>
2026-05-08 16:39:50 +0900

    Add files via upload

--- namilog-sw.js
+++ namilog-sw.js
@@ -1,29 +1,40 @@
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
 self.addEventListener('notificationclick', (event) => {
   event.notification.close();
+
   if (event.action === 'later') return;
 
-  const url = event.notification.data?.url || '/Namilog/?quickCheck=1';
+  const fallbackUrl = new URL('/Namilog/?quickCheck=1', self.location.origin).href;
+  const url = event.notification.data && event.notification.data.url ? event.notification.data.url : fallbackUrl;
 
-  event.waitUntil(
-    clients.matchAll({ type: 'window', includeUncontrolled: true }).then((clientList) => {
-      for (const client of clientList) {
-        try {
-          const clientUrl = new URL(client.url);
-          const targetUrl = new URL(url, self.location.origin);
-          if (clientUrl.origin === targetUrl.origin && clientUrl.pathname.startsWith('/Namilog/')) {
-            client.focus();
-            client.postMessage({ type: 'OPEN_QUICK_CHECK' });
-            return;
-          }
-        } catch (_) {
-          if ('focus' in client) {
-            client.focus();
-            client.postMessage({ type: 'OPEN_QUICK_CHECK' });
-            return;
-          }
+  event.waitUntil((async () => {
+    const clientList = await clients.matchAll({ type: 'window', includeUncontrolled: true });
+    for (const client of clientList) {
+      try {
+        const clientUrl = new URL(client.url);
+        if (clientUrl.origin === self.location.origin && clientUrl.pathname.startsWith('/Namilog/')) {
+          await client.focus();
+          client.postMessage({ type: 'OPEN_QUICK_CHECK' });
+          return;
+        }
+      } catch (_) {
+        if ('focus' in client) {
+          await client.focus();
+          client.postMessage({ type: 'OPEN_QUICK_CHECK' });
+          return;
         }
       }
-      if (clients.openWindow) return clients.openWindow(url);
-    })
-  );
+    }
+
+    if (clients.openWindow) {
+      await clients.openWindow(url);
+    }
+  })());
 });
