8123dd5952fcc8f360fd3a9a60aad9ce3572ff64
Antisanogile <kanaro.sano@gmail.com>
2026-05-08 12:25:20 +0900

    Add files via upload

---
+++ namilog-sw.js
@@ -0,0 +1,25 @@
+self.addEventListener('notificationclick', (event) => {
+  event.notification.close()
+
+  const url = event.notification.data?.url || '/Namilog/?quickCheck=1'
+
+  if (event.action === 'later') {
+    return
+  }
+
+  event.waitUntil(
+    clients.matchAll({ type: 'window', includeUncontrolled: true }).then((clientList) => {
+      for (const client of clientList) {
+        if ('focus' in client) {
+          client.focus()
+          client.postMessage({ type: 'OPEN_QUICK_CHECK' })
+          return
+        }
+      }
+
+      if (clients.openWindow) {
+        return clients.openWindow(url)
+      }
+    }),
+  )
+})
