e18f021fa085006d28611158ad1e678560d9aa62
Antisanogile <kanaro.sano@gmail.com>
2026-05-24 20:31:52 +0900

    Add files via upload

--- namilog-sw.js
+++ namilog-sw.js
@@ -1,4 +1,4 @@
-const VERSION = 'v40.3-minimum-three-choice';
+const VERSION = 'v40.4.4-minimum-three-choice';
 self.addEventListener('install', event => self.skipWaiting());
 self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
 self.addEventListener('notificationclick', event => {
